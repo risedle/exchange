7859d43bc6377aca549dd971b809d1b253b354e7
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-06-06 02:33:08 +0700

    feat: MetaTransactions Feature

---
+++ src/features/MetaTransactions.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import { MetaTransactionsFeature } from "base/features/MetaTransactionsFeature.sol";
+
+/**
+ * @title MetaTransactions
+ * @notice Deploy MetaTransactions using foundry
+ */
+contract MetaTransactions is MetaTransactionsFeature {
+    constructor(address zeroExAddress)
+        public
+        MetaTransactionsFeature(zeroExAddress) {}
+}
