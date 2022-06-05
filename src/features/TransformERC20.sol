fe39ce2987a4f2413ad1765293c55366548fa7fa
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-06-06 01:26:32 +0700

    feat: TransformERC20 Feature

---
+++ src/features/TransformERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import { TransformERC20Feature } from "base/features/TransformERC20Feature.sol";
+
+/**
+ * @title TransformERC20
+ * @notice Deploy TransformERC20 using foundry
+ */
+contract TransformERC20 is TransformERC20Feature {}
